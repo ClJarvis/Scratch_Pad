c6b1d05efa4a1af9d723fa40b5c8d4ad1d7251c0
Chris <clj@ollure.com>
2017-05-15 19:22:03 -0500

    altered the size of canvas vis JS

--- js/scratch.js
+++ js/scratch.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
+canvas.width = window.innerWidth * .80;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = 'white';
 ctx.lineJoin = 'square';
